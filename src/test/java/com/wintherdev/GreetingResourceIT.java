e667c8f863083562e519b7b6579bd201a3c96a7b
Nicklas Waldemar Seier Winther <nsw@DAFOLO.dk>
2026-03-15 16:55:25 +0100

    initial commit

---
+++ src/test/java/com/wintherdev/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.wintherdev;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
